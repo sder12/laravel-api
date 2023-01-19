4b26150834a050de2d3bf26050d43c81f33a408d
sder12 <sara.cetto@gmail.com>
2023-01-19 15:55:32 +0100

    Create function index returning json with projects,techologies and type

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        // $projects = Project::all();
+        $projects = Project::with('type', 'technologies')->get();
+        return  response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
 }
