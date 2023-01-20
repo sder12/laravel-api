23542a989d54005cb4cdf89d41fcda7b006c94fc
sder12 <sara.cetto@gmail.com>
2023-01-20 18:09:38 +0100

    Add paginate to ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,8 @@ class ProjectController extends Controller
     public function index()
     {
         // $projects = Project::all();
-        $projects = Project::with('type', 'technologies')->get();
+        // $projects = Project::with('type', 'technologies')->get();
+        $projects = Project::with('type', 'technologies')->paginate(3);
         return  response()->json([
             'success' => true,
             'results' => $projects
